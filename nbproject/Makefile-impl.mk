a91f6affda119dfcd064411daa2330c92e06c41f
Staffan Johansson <staffanbgj@gmail.com>
2012-12-11 17:08:38 +0100

    cleaned def files

--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=UltraljudSlave.X
+PROJECTNAME=UltraSoundSlave
 
 # Active Configuration
 DEFAULTCONF=default
--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=UltraljudSlave.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/UltraljudSlave.X.production.hex
+CND_ARTIFACT_NAME_default=UltraSoundSlave.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/UltraSoundSlave.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=ultraljudslave.x.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/ultraljudslave.x.tar
+CND_PACKAGE_NAME_default=ultrasoundslave.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/ultrasoundslave.tar
